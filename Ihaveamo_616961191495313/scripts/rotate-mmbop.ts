91d4b354f6cc9ef4a1fe81f053700d6937c34798
InverseVertigo <mrfordegames@gmail.com>
2025-03-17 12:02:51 +1000

    Initial Commit
    Initial Commit

---
+++ Ihaveamo_616961191495313/scripts/rotate-mmbop.ts
@@ -0,0 +1,10 @@
+import * as hz from 'horizon/core';
+
+class rotate_mmbop extends hz.Component<typeof rotate_mmbop> {
+  static propsDefinition = {};
+
+  start() {
+
+  }
+}
+hz.Component.register(rotate_mmbop);
