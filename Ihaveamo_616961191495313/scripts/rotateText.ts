b8a453c4e6384c454d2305895c7b4bb86c79ea48
InverseVertigo <mrfordegames@gmail.com>
2025-03-17 13:48:24 +1000

    tewst
    test

---
+++ Ihaveamo_616961191495313/scripts/rotateText.ts
@@ -0,0 +1,10 @@
+import * as hz from 'horizon/core';
+
+class rotateText extends hz.Component<typeof rotateText> {
+  static propsDefinition = {};
+
+  start() {
+
+  }
+}
+hz.Component.register(rotateText);
